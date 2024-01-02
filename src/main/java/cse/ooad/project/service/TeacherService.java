98f172ed3874c00ce8a4c589392338a5c8eaaa4c
暮心 <1248440677@qq.com>
2024-01-03 06:45:43 +0800

    4.9 patch recover comment

--- src/main/java/cse/ooad/project/service/TeacherService.java
+++ src/main/java/cse/ooad/project/service/TeacherService.java
@@ -478,9 +478,9 @@ public class TeacherService {
                 room.setStatus(Integer.parseInt(strs[9]));
                 room.setFloorId(floor.getFloorId());
                 Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-//                Comment comment = new Comment(null, strs[6], null, room.getRoomId(), 0L, timestamp, false, new ArrayList<Comment>());
-//                comment = commentRepository.save(comment);
-//                room.setCommentBaseId(comment.getCommentId());
+                Comment comment = new Comment(null, strs[6], null, room.getRoomId(), 0L, timestamp, false);
+                comment = commentRepository.save(comment);
+                room.setCommentBaseId(comment.getCommentId());
                 rooms.add(room);
             }
             roomRepository.saveAll(rooms);
